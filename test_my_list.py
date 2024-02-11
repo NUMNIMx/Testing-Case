543c1324862428f9ced77632fe7b5d4d9b3566e9
NUMNIMx <itspeemgmth@gmail.com>
2024-02-11 22:09:39 +0700

    update test case info

--- test_my_list.py
+++ test_my_list.py
@@ -3,32 +3,28 @@ import unittest
 
 class MyListTest(unittest.TestCase):
     def test_find_max(self):
-        """
-        ทดสอบฟังก์ชัน find_max
-        """
         self.assertEqual(find_max([1, 2, 3]), 3)
         self.assertEqual(find_max([5, 10, 2]), 10)
         self.assertEqual(find_max([100, 50, 25]), 100)
 
     def test_find_min(self):
-        """
-        ทดสอบฟังก์ชัน find_min
-        """
         self.assertEqual(find_min([1, 2, 3]), 1)
         self.assertEqual(find_min([5, 10, 2]), 2)
         self.assertEqual(find_min([100, 50, 25]), 25)
 
     def test_find_average(self):
-        """
-        ทดสอบฟังก์ชัน find_average
-        """
         self.assertEqual(find_average([4, 6, 5]), 5)
+        self.assertEqual(find_average([74, 52, 99]), 75)
+        self.assertEqual(find_average([90, 40, 50 , 45 , 45]), 54)
 
     def test_is_sorted_with_sorted_list(self):
         self.assertTrue(is_sorted([1, 2, 3, 4, 5]))
         self.assertTrue(is_sorted([25, 71 ,77 ,80 ]))
+        self.assertTrue(is_sorted([14, 54 ,99 ,1150 ]))
 
     def test_reverse_list(self):
         self.assertEqual(reverse_list([1, 2, 3]), [3, 2, 1])
+        self.assertEqual(reverse_list([20, 4, 1]), [1, 4, 20])
+        self.assertEqual(reverse_list([111, 898, 74]), [74, 898, 111])
 
 
